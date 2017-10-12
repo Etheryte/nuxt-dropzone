bba051b386fd0dd28e53e2d810df086d17a73c53
Etheryte <eth@mango.local>
2017-10-12 14:21:00 +0300

    Initial work

---
+++ demo/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+	<dropzone id="foo" :url="'http://httpbin.org/anything'">
+	</dropzone>
+</template>
+<script>
+import Dropzone from 'nuxt-dropzone'
+
+export default {
+	components: {
+		Dropzone
+	}
+}
+</script>
---
+++ index.js
@@ -0,0 +1,21 @@
+let component = {}
+
+if (process.browser) {
+	component = require('vue2-dropzone')
+}
+component.name = 'dropzone'
+component.render = function (createElement) {
+	const that = this._self
+	return createElement('form', {
+		props: that.props,
+		attrs: {
+			class: 'vue-dropzone dropzone',
+			id: that.id || '',
+			action: that.url || '',
+			dropzoneOptions: that.dropzoneOptions,
+			useCustomDropzoneOptions: that.useCustomDropzoneOptions
+		}
+	}, this.$slots.default)
+}
+
+export default component
