1fd8dc9731e7d0fc9d9fd5a1b2130f6a61e16bf4
Etheryte <eth@Mango.local>
2017-11-06 23:15:44 +0200

    Fix old editor config indentation

--- demo/pages/index.vue
+++ demo/pages/index.vue
@@ -1,25 +1,25 @@
 <template>
-	<dropzone id="foo" ref="el" :options="options" :destroyDropzone="true"></dropzone>
+    <dropzone id="foo" ref="el" :options="options" :destroyDropzone="true"></dropzone>
 </template>
 <script>
 import Dropzone from 'nuxt-dropzone'
 import 'nuxt-dropzone/dropzone.css'
 
 export default {
-	components: {
-		Dropzone
-	},
-	data() {
-		return {
-			// See https://rowanwins.github.io/vue-dropzone/docs/dist/index.html#/props
-			options: {
-				url: "http://httpbin.org/anything"
-			}
-		}
-	},
-	mounted() {
-		// Everything is mounted and you can access the dropzone instance
-		const instance = this.$refs.el.dropzone
-	}
+    components: {
+        Dropzone
+    },
+    data() {
+        return {
+            // See https://rowanwins.github.io/vue-dropzone/docs/dist/index.html#/props
+            options: {
+                url: "http://httpbin.org/anything"
+            }
+        }
+    },
+    mounted() {
+        // Everything is mounted and you can access the dropzone instance
+        const instance = this.$refs.el.dropzone
+    }
 }
 </script>
--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 var component = {}
 
 if (process.browser) {
-	component = require('vue2-dropzone')
+    component = require('vue2-dropzone')
 }
 component.name = 'dropzone'
 component.render = function (createElement) {
-	const that = this._self
-	return createElement('div', {
-		props: that.props,
-		attrs: {
-			class: 'vue-dropzone dropzone',
-			id: that.id || ''
-		},
-		ref: 'dropzoneElement'
-	}, this.$slots.default)
+    const that = this._self
+    return createElement('div', {
+        props: that.props,
+        attrs: {
+            class: 'vue-dropzone dropzone',
+            id: that.id || ''
+        },
+        ref: 'dropzoneElement'
+    }, this.$slots.default)
 }
 
 module.exports = component
