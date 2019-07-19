b0d955cd55669ef0799d44e2027db704065fe63f
Wicpar <frederic.artus.nieto@gmail.com>
2019-07-19 18:28:05 +0200

    Update index.js
    fixed useCustomSlots not properly syncing with ssr

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-var component = {}
-
+let component = {};
+component.props = ["useCustomSlot"];
 if (process.browser) {
-    component = require('vue2-dropzone/dist/vue2Dropzone.js')
+  component = require('vue2-dropzone/dist/vue2Dropzone.js')
 }
-component.name = 'dropzone'
 component.render = function (createElement) {
-    var that = this._self
-    return createElement('div', {
-        props: that.props,
-        attrs: {
-            class: 'vue-dropzone dropzone',
-            id: that.id || ''
-        },
-        ref: 'dropzoneElement'
-    }, [this.useCustomSlot ? createElement("div", {staticClass: "dz-message"}, [this.$slots.default]) : this.$slots.default])
-}
+  const that = this._self;
+  return createElement('div', {
+    props: that.props,
+    attrs: {
+      class: 'vue-dropzone dropzone',
+      id: that.id || ''
+    },
+    ref: 'dropzoneElement'
+  }, [this.useCustomSlot === "" || this.useCustomSlot === true ? createElement("div", {staticClass: "dz-message"}, [this._t("default", [this._v("Drop files here to upload")])], 2) : this._e()])
+};
+component.name = 'dropzone';
 
-module.exports = component
+export default component;
